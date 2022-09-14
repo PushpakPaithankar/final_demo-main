6166ea3a6d3e0f8a0338274ae82bce446fd8b201
PushpakPaithankar <111685947+PushpakPaithankar@users.noreply.github.com>
2022-09-14 16:51:38 +1000

    Update initial_setup.sh

--- setup/initial_setup.sh
+++ setup/initial_setup.sh
@@ -1,5 +1,5 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="ment360live-beta
+export PROJECT_ID="trial-liveability-123"
       
 #2.Sets the project,location &dataset
 gcloud config set project ${PROJECT_ID}
